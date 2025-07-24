846f84cd42e862741e68ec66d37d2b69961d7a8a
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-07-24 13:05:30 -0400

    Generating a Random color;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,15 @@ export default function Home() {
   const [answer, setAnswer] = useState('#FFFFFF');
 
   const randomColor = (): string => {
-    let color = '#000000';
+    let color = '#';
+
+    const letters = '0123456789ABCDEF';
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    console.log('color', color);
 
     return color;
   }
